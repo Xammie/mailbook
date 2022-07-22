beb52f8e8f2546d2ec32f253559fbd9bc1b34471
Max Hoogenbosch <mhoogenbosch@enrise.com>
2022-07-22 22:10:03 +0200

    Added background when using display preview

--- resources/views/display.blade.php
+++ resources/views/display.blade.php
@@ -1,7 +1,7 @@
 <div class="flex flex-col flex-1">
-    <div class="bg-white text-black flex-1 flex justify-center">
+    <div class="bg-gray-900 text-black flex-1 flex justify-center">
         <iframe @class([
-                    'w-full h-full',
+                    'w-full h-full bg-white',
                     'max-w-md' => $display === 'phone',
                     'max-w-3xl' => $display === 'tablet',
                 ])
