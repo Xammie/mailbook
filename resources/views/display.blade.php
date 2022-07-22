cb8c700a0dffcac6959f062fd974d6a86cba0755
Max Hoogenbosch <mhoogenbosch@enrise.com>
2022-07-22 17:56:38 +0200

    WIP device preview

--- resources/views/display.blade.php
+++ resources/views/display.blade.php
@@ -1,5 +1,10 @@
 <div class="flex flex-col flex-1">
-    <div class="bg-white text-black flex-1">
-        <iframe class="w-full h-full" src="{{ route('mailbook.content', ['class' => $current->class(), 'variant' => $current->currentVariant()?->slug]) }}"></iframe>
+    <div class="bg-white text-black flex-1 flex justify-center">
+        <iframe @class([
+                    'w-full h-full',
+                    'max-w-md' => request()->get('display') === 'phone',
+                    'max-w-3xl' => request()->get('display') === 'tablet',
+                ])
+                src="{{ route('mailbook.content', ['class' => $current->class(), 'variant' => $current->currentVariant()?->slug]) }}"></iframe>
     </div>
 </div>
