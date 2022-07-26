980d2313fd2290226255e076c7bc30b0397ed3db
Max Hoogenbosch <mhoogenbosch@enrise.com>
2022-07-26 18:30:42 +0200

    Added middlewares config option

--- config/mailbook.php
+++ config/mailbook.php
@@ -1,8 +1,11 @@
 <?php
 
 return [
-    'route_prefix' => '/mailbook',
     'database_rollback' => true,
     'display_preview' => true,
     'refresh_button' => true,
+    'route_prefix' => '/mailbook',
+    'middlewares' => [
+        Xammie\Mailbook\Http\Middlewares\RollbackDatabase::class,
+    ],
 ];
--- routes/routes.php
+++ routes/routes.php
@@ -3,10 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use Xammie\Mailbook\Http\Controllers\DashboardController;
 use Xammie\Mailbook\Http\Controllers\MailContentController;
-use Xammie\Mailbook\Http\Middlewares\RollbackDatabase;
+
 
 Route::prefix(config('mailbook.route_prefix'))
-    ->middleware(RollbackDatabase::class)
+    ->middleware(config('mailbook.middlewares'))
     ->group(function () {
         Route::get('/', DashboardController::class)->name('mailbook.dashboard');
         Route::get('/content/{class}/{variant?}', MailContentController::class)->name('mailbook.content');
