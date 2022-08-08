1432242fa4809996ba9a745f830de44b6bf211cd
Max Hoogenbosch <mhoogenbosch@enrise.com>
2022-08-08 15:50:47 +0200

    Fixed tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -27,10 +27,6 @@ class TestCase extends Orchestra
     public function getEnvironmentSetUp($app)
     {
         config()->set('database.default', 'testing');
-
-        /*
-        $migration = include __DIR__.'/../database/migrations/create_mailbook_table.php.stub';
-        $migration->up();
-        */
+        config()->set('mailbook.enabled', true);
     }
 }
