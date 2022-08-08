e8965048b12afea232275237b7e1fc760213bc3f
Max Hoogenbosch <mhoogenbosch@enrise.com>
2022-08-08 16:19:29 +0200

    Removed factory setup

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,21 +2,11 @@
 
 namespace Xammie\Mailbook\Tests;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
 use Xammie\Mailbook\MailbookServiceProvider;
 
 class TestCase extends Orchestra
 {
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'Xammie\\Mailbook\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
-    }
-
     protected function getPackageProviders($app)
     {
         return [
