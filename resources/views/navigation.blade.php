53f255903d38d6c750fe1233d72727d91774069b
Max Hoogenbosch <mhoogenbosch@enrise.com>
2023-05-17 17:42:16 +0200

    Fixed send button active state
    This fixes an issue where the send button would be active with the phone display preview.

--- resources/views/navigation.blade.php
+++ resources/views/navigation.blade.php
@@ -39,7 +39,8 @@
                            'bg-gray-600 text-white' => $display === 'phone',
                            'hover:bg-gray-700' => $display !== 'phone',
                        ])
-                       aria-label="Phone" title="Phone">
+                       aria-label="Phone"
+                       title="Phone">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
                              stroke="currentColor" stroke-width="2">
                             <path stroke-linecap="round" stroke-linejoin="round"
@@ -54,7 +55,8 @@
                             'bg-gray-600 text-white' => $display === 'tablet',
                             'hover:bg-gray-700' => $display !== 'tablet',
                        ])
-                       aria-label="Tablet" title="Tablet">
+                       aria-label="Tablet"
+                       title="Tablet">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
                              stroke="currentColor" stroke-width="2">
                             <path stroke-linecap="round" stroke-linejoin="round"
@@ -69,7 +71,8 @@
                             'bg-gray-600 text-white' => $display === null,
                             'hover:bg-gray-700' => $display !== null,
                        ])
-                       aria-label="Desktop" title="Desktop">
+                       aria-label="Desktop"
+                       title="Desktop">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
                              stroke="currentColor" stroke-width="2">
                             <path stroke-linecap="round" stroke-linejoin="round"
@@ -81,12 +84,9 @@
             @if($send)
                 <div class="p-2">
                     <a href="{{ route('mailbook.send', ['class' => $current->class(), 'variant' => $current->currentVariant()?->slug, 'locale' => $currentLocale]) }}"
-                       @class([
-                           'flex items-center justify-center p-3 rounded-full  transition-colors duration-100',
-                           'bg-gray-600 text-white' => $display === 'phone',
-                           'hover:bg-gray-700' => $display !== 'phone',
-                       ])
-                       aria-label="Send" title="Send">
+                       class="flex items-center justify-center p-3 rounded-full  transition-colors duration-100 hover:bg-gray-700"
+                       aria-label="Send"
+                       title="Send">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-6 h-6 fill-white">
                             <path
                                 d="M498.1 5.6c10.1 7 15.4 19.1 13.5 31.2l-64 416c-1.5 9.7-7.4 18.2-16 23s-18.9 5.4-28 1.6L284 427.7l-68.5 74.1c-8.9 9.7-22.9 12.9-35.2 8.1S160 493.2 160 480V396.4c0-4 1.5-7.8 4.2-10.7L331.8 202.8c5.8-6.3 5.6-16-.4-22s-15.7-6.4-22-.7L106 360.8 17.7 316.6C7.1 311.3 .3 300.7 0 288.9s5.9-22.8 16.1-28.7l448-256c10.7-6.1 23.9-5.5 34 1.4z"/>
