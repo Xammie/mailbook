9438b02bb94010453da4edee94597c46e13cd200
Max Hoogenbosch <mhoogenbosch@enrise.com>
2024-02-11 12:21:16 +0100

    Use mail envelope for example

--- stubs/MailbookMail.php
+++ stubs/MailbookMail.php
@@ -3,14 +3,24 @@
 namespace App\Mail;
 
 use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Address;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
 
 class MailbookMail extends Mailable
 {
-    public function build(): self
+    public function envelope(): Envelope
     {
-        return $this
-            ->subject('Welcome to mailbook!')
-            ->from('laravel@mailbook.dev', 'Mailbook')
-            ->markdown('mail.mailbook');
+        return new Envelope(
+            from: new Address('laravel@mailbook.dev', 'Mailbook'),
+            subject: 'Welcome to mailbook!',
+        );
+    }
+
+    public function content(): Content
+    {
+        return new Content(
+            markdown: 'mail.mailbook',
+        );
     }
 }
