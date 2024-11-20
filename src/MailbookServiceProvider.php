bc761b3fe7c2db3d5f336fc18e2d879be2b39d1f
Max Hoogenbosch <mhoogenbosch@enrise.com>
2024-11-20 18:21:23 +0100

    Formatting

--- src/MailbookServiceProvider.php
+++ src/MailbookServiceProvider.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Xammie\Mailbook;
 
 use Illuminate\Mail\MailManager;
-use Illuminate\Support\Facades\Mail;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 use Xammie\Mailbook\Commands\InstallMailbookCommand;
@@ -25,9 +24,7 @@ class MailbookServiceProvider extends PackageServiceProvider
     public function packageBooted(): void
     {
         $this->app->extend('mail.manager', function (MailManager $manager): MailManager {
-            return $manager->extend('mailbook', function (): MailbookTransport {
-                return new MailbookTransport();
-            });
+            return $manager->extend('mailbook', fn () => new MailbookTransport);
         });
     }
 }
